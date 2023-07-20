177cd928efa4c0efb88136ea05c335bb2e2b1bb6
bishtk435 <bishtk435@gmail.com>
2023-07-21 04:34:11 +0530

    added chatgpt and web speech API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SpeakBotContainerComponent } from './speak-bot-container/speak-bot-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeakBotContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/env.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    "CHAT_GPT_URL_V1": "https://api.openai.com/v1/",
+    "OPEN_AI_KEY": "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",
+    "OEPN_API_ORG_ID": "org-HWRy3ZoPhQ2YbWGlaZg1qTeM",
+}
